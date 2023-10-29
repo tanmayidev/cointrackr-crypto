888d40d9a98ab7f7d3882f251a7daa9b51a68879
Tanmayi D <60173146+tanmayidev@users.noreply.github.com>
2023-10-29 18:59:42 +0530

    feat: create pending styles and index html files
    Add missing `index.html` file and global styles

---
+++ src/config/constants.ts
@@ -0,0 +1,2 @@
+export const API_V3_URL = "https://api.coingecko.com/api/v3";
+export const COINS_MARKETS_API_URL = `${API_V3_URL}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=4&page=1&sparkline=false`;
