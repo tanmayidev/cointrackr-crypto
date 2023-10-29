00998540cbb0ffd1edabf145039762055e3ed77b
Tanmayi D <60173146+tanmayidev@users.noreply.github.com>
2023-10-29 18:34:10 +0530

    feat: create entry-points and their dependencies.
    Create App, index, screens, components and dist.

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import Home from "./screens/Home";
+import "../src/dist/styles.css";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Coin from "./screens/Coin";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/coin" element={<Coin />}>
+          <Route path=":coinId" element={<Coin />}></Route>
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
